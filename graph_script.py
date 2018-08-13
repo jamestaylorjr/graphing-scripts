c1a8fa567fd472fc0a2c41b947fed0d97ed7d291
James Taylor <jimtaylor@tamu.edu>
2018-08-13 15:56:30 -0500

    Added new features

--- graph_script.py
+++ graph_script.py
@@ -11,16 +11,18 @@ import numpy as np
 
 parser = ArgumentParser()
 parser.add_argument(dest="filename", help="The input datafile", metavar="FILE")
-parser.add_argument(dest="plottype",help="The type of plot")
+parser.add_argument(dest="plottype",help="The type of plot. Currently only supports bar and line arguments.")
 parser.add_argument(dest="group", help="The groups that you would like to sort by.")
 parser.add_argument(dest="dep", help="The dependent variable you wish to graph.")
 parser.add_argument("-t","--title",dest="title", help="The title of your figure.")
 parser.add_argument(dest="output", help="The desired output filename. Please include the extension.")
+parser.add_argument("-c","--covariate",dest="covar",help="a covariate to make a subgroup for graphing.")
 args = parser.parse_args()
 
 
 data1 = pd.read_csv(args.filename, sep="\t", lineterminator="\n")
 
+colors = ['#1095FF','#474999','#2127CC','#FFA250','#CC5821']
 #print(data1.head())
 
 def plot_v1(data,error,title):
@@ -39,7 +41,6 @@ def plot_v1(data,error,title):
 	maxmean = max(data)
 	maxstd = max(error)
 	ylimval = (maxmean+maxstd)*1.1
-	print(ylimval)
 	#setting a nice y-axis limit
 	ax.set_ylim(0,ylimval)
 
@@ -59,25 +60,40 @@ def plot_v2(data):
 		plt.style.use('seaborn-paper')
 		matplotlib.rc("font",family="serif")
 	set_style()
-	matrix1 = data[data.columns[0]].values
-	list1 = matrix1.tolist()
-	matrix2 = data[data.columns[1]].values
-	list2 = matrix2.tolist()
-	fig = plt.figure() 
-	plt.plot(list1,list2)
+	fig = plt.figure()
+	if args.covar is not None:
+		splitvar = args.covar.split(',')
+		data['covar'] = ""
+		for name in splitvar:
+			data['covar'] = data['covar'] + data[name].astype(str)
+		test = data.groupby('covar')
+		for name, group in test:
+			test2 = group.groupby(args.group,sort=False)[args.dep].mean()
+			std = group.groupby(args.group,sort=False)[args.dep].std()
+			plt.plot(test2.index,test2,label=name)
+			plt.errorbar(test2.index,test2,yerr=std)
+	else:
+		math = data.groupby(args.group,sort=False)[args.dep].mean()
+		plt.plot(math.index,math)
+
+  
+	#handles, labels = plt.get_legend_handles_labels()
+	handles = ""
+	plt.legend(loc='upper left')
 	plt.title(args.title)
 	plt.xlabel(args.group)
 	plt.ylabel(args.dep)
 
 	return fig
 
+	
 if args.plottype == "bar":
-	means = data1.groupby(args.group)[args.dep].mean()
+	means = data1.groupby(args.group,sort=False)[args.dep].mean()
 	#print(means)
-	data_std = data1.groupby(args.group)[args.dep].std()
+	data_std = data1.groupby(args.group, sort=False)[args.dep].std()
 	#print(data_std)
 	groups = means.index
-
+	print(groups)
 	p1=plot_v1(means,data_std,args.title)
 	p1[0].savefig(args.output)
 elif args.plottype == "line":
